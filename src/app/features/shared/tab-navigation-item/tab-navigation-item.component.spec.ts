16d7a9e58beca18936a6eb365675e76f2572efe4
Harrison Willett <harrison.willett@avanade.com>
2020-04-01 00:35:41 -0500

    Tab Nav Item -  Coverage 100% using router testing module

--- src/app/features/shared/tab-navigation-item/tab-navigation-item.component.spec.ts
+++ src/app/features/shared/tab-navigation-item/tab-navigation-item.component.spec.ts
@@ -1,25 +1,85 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Routes, Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 import { TabNavigationItemComponent } from './tab-navigation-item.component';
+import { Component } from '@angular/core';
+
+@Component({
+  template: ``
+})
+export class StubPageComponent { }
+
+const routes: Routes = [
+  { path: "game", component: StubPageComponent },
+  //Sheet Feature
+  { path: "sprites", component: StubPageComponent },
+  {
+    path: "sprites/create-sprite-sheet",
+    component: StubPageComponent
+  },
+  {
+    path: "sprites/:sheetId",
+    component: StubPageComponent
+  },
+  //Sprite Feature
+  {
+    path: "sprites/:sheetId/:spriteId",
+    component: StubPageComponent
+  },
+  //Music Feature
+  { path: "music-tracks", component: StubPageComponent },
+  { path: "", redirectTo: "/game", pathMatch: "full" }
+];
 
 describe('TabNavigationItemComponent', () => {
   let component: TabNavigationItemComponent;
   let fixture: ComponentFixture<TabNavigationItemComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TabNavigationItemComponent ]
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [TabNavigationItemComponent]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+      .compileComponents();
     fixture = TestBed.createComponent(TabNavigationItemComponent);
     component = fixture.componentInstance;
+    component.tab = {
+      title: "Edit Sprites",
+      routerLink: "/sprites",
+      routerLinkActiveOptions: { exact: false }
+    };
+    router = TestBed.get(Router);
     fixture.detectChanges();
-  });
+  }));
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('should have a "string" for a title', () => {
+    expect(typeof component.tab.title).toBe('string');
+  });
+
+  fit('should have active link styles when page matches tab', fakeAsync(() => {
+    fixture.ngZone.run(() => {
+      router.navigate(['sprites']);
+      tick();
+      fixture.detectChanges();
+      let link = fixture.debugElement.queryAll(By.css('.nav-link'));
+      expect(link[0].nativeNode.classList.value).toBe("nav-link active");
+    });
+  }));
+
+  fit('should NOT have active link styles when page does NOT match tab', fakeAsync(() => {
+    fixture.ngZone.run(() => {
+      router.navigate(['game']);
+      tick();
+      fixture.detectChanges();
+      let link = fixture.debugElement.queryAll(By.css('.nav-link'));
+      expect(link[0].nativeNode.classList.value).toBe("nav-link");
+    });
+  }));
 });
